1c0e50d91d0b08add00156789e78a1cdd9d86897
Denis <seltix9381@gmail.com>
2024-07-09 22:00:37 +0300

    add changes 3

--- catalog/models.py
+++ catalog/models.py
@@ -17,8 +17,13 @@ class Product(models.Model):
     name = models.CharField(max_length=100, verbose_name='Наименование')
     description = models.TextField(verbose_name='Описание')
     image = models.ImageField(upload_to='image', blank=True, null=True, verbose_name='Изображение')
-    category = models.ForeignKey(Category, on_delete=models.SET_NULL, blank=True, null=True, related_name='Category')
-    price = models.IntegerField(verbose_name='Цена за покупку')
+    category = models.ForeignKey(Category,
+                                 on_delete=models.SET_NULL,
+                                 blank=True,
+                                 null=True,
+                                 related_name='Category',
+                                 verbose_name='Категория')
+    price = models.IntegerField(blank=True, null=True, verbose_name='Цена за покупку')
     created_at = models.DateTimeField(blank=True, null=True, verbose_name='Дата создания')
     updated_at = models.DateTimeField(blank=True, null=True, verbose_name='Дата последнего изменения')
 
