f81a6ed3e80d5e3d54ee77378e7374d8cfa45acc
Denis <seltix9381@gmail.com>
2024-07-09 20:02:10 +0300

    add admin.py

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from catalog.models import Category, Product
 
-# Register your models here.
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name')
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'category')
+    list_filter = ('category',)
+    search_fields = ('name', 'description',)
+
+# Для моделей категории и продукта настройте отображение в административной панели. Для категорий выведите id
+# и наименование в список отображения, а для продуктов выведите в список id, название, цену и категорию.
+#
+# При этом интерфейс вывода продуктов настройте так, чтобы можно было результат отображения фильтровать
+# по категории, а также осуществлять поиск по названию и полю описания.
--- config/urls.py
+++ config/urls.py
@@ -6,4 +6,4 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('catalog.urls', namespace='catalog'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # запиши!
+]  # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)  # запиши!
