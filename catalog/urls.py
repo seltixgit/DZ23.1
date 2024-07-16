6e91c9b8a2a4a7f34c7625fc28d7ba4b854824aa
Denis <seltix9381@gmail.com>
2024-07-16 23:46:03 +0300

    commit works 2

--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from catalog.apps import CatalogConfig
-from catalog.views import products_list
+from catalog.views import products_list, product_detail
 
 app_name = CatalogConfig.name
 
 urlpatterns = [
-    path('', products_list, name='products_list')
+    path('', products_list, name='products_list'),
+    path('catalog/<int:pk>/', product_detail, name='product_detail')
 ]
