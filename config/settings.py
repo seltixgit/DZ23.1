c8424047dde430df57e3d7a44d19e3620cda8f55
Denis <seltix9381@gmail.com>
2024-08-13 17:18:18 +0300

    task 1 is completed

--- config/settings.py
+++ config/settings.py
@@ -17,6 +17,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'catalog',
     'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -94,3 +95,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+AUTH_USER_MODEL = 'users.User'
